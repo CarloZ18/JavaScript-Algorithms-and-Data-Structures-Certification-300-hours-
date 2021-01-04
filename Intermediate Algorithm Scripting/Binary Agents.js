d9ca97eb1f6e0434d8537c40782b058dad6c39d8
Your Name <you@example.com>
2021-01-04 15:16:02 -0400

    Binary Agents

---
+++ Intermediate Algorithm Scripting/Binary Agents.js
@@ -0,0 +1,16 @@
+//Return an English translated sentence of the passed binary string.
+//The binary string will be space separated.
+
+function binaryAgent(str) {
+    //Indico 2 variables la cadena binaria (esta la divido por conjunto de números binarios) y la cadena de valores unicode
+    var biString = str.split(" ");
+    var uniString = [];
+  
+    // itero entre cada conjunto de numeros binarios y voy pasando cada uno a numero decimal, luego estos los transformo en letra con fromCharCode()
+    for (var i = 0; i < biString.length; i++) {
+      uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
+    }
+    console.log(uniString);
+    return uniString.join(""); //Vuelvo a unir las palabras para que se forme la oración
+  }
+  
