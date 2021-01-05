43a85610271d9ce8a97b5c176fdf165bda642436
Your Name <you@example.com>
2021-01-05 18:42:16 -0400

    Arguments Optional

--- Intermediate Algorithm Scripting/Arguments Optional.js
+++ Intermediate Algorithm Scripting/Arguments Optional.js
@@ -1,3 +1,4 @@
+   
 //Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum. For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function. Calling this returned function with a single argument will then return the sum: var sumTwoAnd = addTogether(2); sumTwoAnd(3) returns 5. If either argument isn't a valid number, return undefined.
 
 function addTogether(a, b) {
@@ -9,4 +10,6 @@ function addTogether(a, b) {
     const sum = (b) => (typeof b === "number" ? a + b : undefined);
     return typeof b === "undefined" ? (b) => sum(b) : sum(b);
   }
-  
+   
+// NOTA: entendi toda la logica, pero tengo una duda Jainer en la linea 10 que coloque (b) => sum(b) : sum(b); No termino de comprender a que se refiere esa parte, vi esa respuesta y la ajuste con mis argumentos y tal, porque me parecio que estaba chevere, pero esa parte no termino de comprenderla, plis, HELP. 
+// Yo asumo que al no tener definido el argumento b, en la funcion, esto lo que dice es que toma el proximo para ejecutar la suma, y lo coloca como argumento b, para remplezar ese espacio vacio.
